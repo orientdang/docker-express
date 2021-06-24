88ad527ec220e3962eb0dba120c281e2efcf4381
dang-codelink <dang@codelink.io>
2021-06-24 14:09:33 +0700

    connect to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const port = process.env.PORT || 3000
+
+mongoose.connect("mongodb://dangnh:dangnhpass@mongo:27017/?authSource=admin")
+				.then(()=> console.log('Connect to DB successfull'))
+				.catch((e) => console.log(e))
 
 app.get('/', (req, res) =>{
 	res.send('Hi Daniel change!!!')
